80be10977d124041824f538d2dc55b1ee8f3472a
Anirudh Jagadish <anirudh101298@gmail.com>
2024-02-12 20:19:01 -0500

    trying ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ const testimonialsModalFunc = function () {
 
 // add click event to modal close button
 // modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+// overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
